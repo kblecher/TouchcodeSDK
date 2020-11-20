0692c8558342569a2f8a332dff6047838eb8fa70
Kathryn <kathrynb@avi.com>
2020-11-20 09:16:22 -0500

    add framework

---
+++ touchcodeSDK.xcframework/ios-x86_64-simulator/touchcodeSDK.framework/Headers/testSDK.h
@@ -0,0 +1,19 @@
+//
+//  testSDK.h
+//  testSDK
+//
+//  Copyright © 2018 T-ink. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for testSDK.
+FOUNDATION_EXPORT double testSDKVersionNumber;
+
+//! Project version string for testSDK.
+FOUNDATION_EXPORT const unsigned char testSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like//
+
+
+
